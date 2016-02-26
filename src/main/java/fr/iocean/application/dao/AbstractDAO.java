ae7d56f098f350f5c2ce1d6fd9f92b7161989212
Ahmed Rafik <rafik_ahmed@outlook.fr>
2016-02-26 12:23:25 +0100

    abstractdao & IoEntity

---
+++ src/main/java/fr/iocean/application/dao/AbstractDAO.java
@@ -0,0 +1,51 @@
+package fr.iocean.application.dao;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.hibernate.Session;
+import org.springframework.stereotype.Repository;
+
+import fr.iocean.application.model.IoEntity;
+
+@Repository
+public abstract class AbstractDAO <T extends IoEntity> {
+	
+	
+	private Class<T> entityClass;
+	
+	@PostConstruct
+	public void initEntityClass(){
+		entityClass = getEntityClass();
+	}
+	
+	protected abstract Class<T> getEntityClass();
+	
+	@PersistenceContext
+	protected EntityManager em;
+
+	protected Session getSession() {
+		return em.unwrap(Session.class);
+	}
+	
+	public void save(T entity){
+		em.persist(entity);
+	}
+	
+	public T findOne(Long id){
+		return em.find(entityClass, id);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<T> findAll() {
+		return getSession().createCriteria(entityClass).list();
+	}
+	
+	public void delete(T entity){
+		em.remove(entity);
+	}
+	
+}
---
+++ src/main/java/fr/iocean/application/model/IoEntity.java
@@ -0,0 +1,11 @@
+package fr.iocean.application.model;
+
+import java.io.Serializable;
+
+public interface IoEntity extends Serializable {
+
+	Long getId();
+	
+	public void setId(Long id);
+	
+}
