033786169a5b143cfff8fbbd30db71a25ef7bd47
Ahmed Rafik <rafik_ahmed@outlook.fr>
2016-02-26 16:11:24 +0100

    injection EmpruntDAO dans Service

--- src/main/java/fr/iocean/application/service/EmpruntService.java
+++ src/main/java/fr/iocean/application/service/EmpruntService.java
@@ -1,10 +1,13 @@
 package fr.iocean.application.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import fr.iocean.application.dao.EmpruntDAO;
 import fr.iocean.application.model.Emprunt;
 
 public class EmpruntService extends AbstractService<Emprunt, EmpruntDAO>{
 
+	@Autowired
 	private EmpruntDAO empruntDao;
 
 	@Override
